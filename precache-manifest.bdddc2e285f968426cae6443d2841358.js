dea56c5c0ccce785cf015738b3cc17008e81dbed
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-29 14:06:37 +0100

    Updates

---
+++ precache-manifest.bdddc2e285f968426cae6443d2841358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1b764c7cfd1f6e2558cf9d774f7c10",
+    "url": "/event-brite-react/index.html"
+  },
+  {
+    "revision": "08f825132579878da6d1",
+    "url": "/event-brite-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d1da7eddec6c932c025",
+    "url": "/event-brite-react/static/js/2.23d1d002.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/event-brite-react/static/js/2.23d1d002.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08f825132579878da6d1",
+    "url": "/event-brite-react/static/js/main.bb2e6238.chunk.js"
+  },
+  {
+    "revision": "cb4459dad5b1ec66d852",
+    "url": "/event-brite-react/static/js/runtime-main.d01f6151.js"
+  }
+]);
